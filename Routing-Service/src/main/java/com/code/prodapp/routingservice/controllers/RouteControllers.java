03eb6b6e809d7830a01dfe3405df48b553664947
AamirrH <aamirr.1704@gmail.com>
2026-06-24 20:13:00 +0530

    async : test order-place event

--- Routing-Service/src/main/java/com/code/prodapp/routingservice/controllers/RouteControllers.java
+++ Routing-Service/src/main/java/com/code/prodapp/routingservice/controllers/RouteControllers.java
@@ -2,10 +2,13 @@ package com.code.prodapp.routingservice.controllers;
 
 import com.code.prodapp.routingservice.DTOs.RouteRequestDTO;
 import com.code.prodapp.routingservice.DTOs.RouteResponseDTO;
+import com.code.prodapp.routingservice.DTOs.RouteServiceDTO;
 import com.code.prodapp.routingservice.services.RoutingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/routes")
@@ -15,7 +18,7 @@ public class RouteControllers {
     private final RoutingService routingService;
 
     @PostMapping
-    public ResponseEntity<RouteResponseDTO> getAllRoutes(@RequestBody RouteRequestDTO routeRequestDTO) {
+    public ResponseEntity<List<RouteServiceDTO>> getAllRoutes(@RequestBody RouteRequestDTO routeRequestDTO) {
         return ResponseEntity.ok(routingService.getAllRoutes(routeRequestDTO));
     }
 
