c0f960688f036765713da1ce8225acb3ec3a02d0
AamirrH <aamirr.1704@gmail.com>
2026-07-04 16:54:55 +0530

    feat :add SelectedRoute entity annotations and specifications

--- Routing-Service/src/main/java/com/code/prodapp/routingservice/entities/SelectedRoute.java
+++ Routing-Service/src/main/java/com/code/prodapp/routingservice/entities/SelectedRoute.java
@@ -1,9 +1,6 @@
 package com.code.prodapp.routingservice.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,28 +14,45 @@ import java.util.UUID;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "response_routes")
 public class SelectedRoute {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "response_uuid")
     private UUID serialId;
 
+    @Column(name = "selected_route_id",nullable = false)
     private Long selectedRouteId;
 
+    @Column(name = "customer_id",nullable = false)
     private Long customerId;
 
+    @Column(name = "order_id",nullable = false)
     private Long orderId;
 
+    @ElementCollection
+    @CollectionTable(name="coordinates_table",joinColumns = @JoinColumn(referencedColumnName = "response_uuid" ,
+    name = "selected_route_uuid"))
+    @Column(name = "customer_coordinates")
     private List<Double> customerCoordinates;
 
     private String customerAddress;
 
+    @ElementCollection
+    @CollectionTable(name = "warehouse_coordinates_table",joinColumns = @JoinColumn(referencedColumnName = "response_uuid",
+    name = "warehouse_coordinate_table_response_uuid" ))
+    @Column(name = "warehouse_coordinates")
     private List<Double> wareHouseCoordinates;
 
+    @Column(name = "kilometers_to_cover",nullable = false)
     private double totalDistance;
 
+    @Column(name = "minutes_to_reach",nullable = false)
     private double timeToReach;
 
+
+    @Column(name = "model_reasoning",nullable = false,length = 100)
     private String reasoning;
 
 }
