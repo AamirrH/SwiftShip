fdd96edb29905c80a2f4852b87c368b23fda7ef0
AamirrH <aamirr.1704@gmail.com>
2026-07-13 02:24:50 +0530

    fix: handle order service exceptions

---
+++ Order-Service/src/main/java/com/code/prodapp/orderservice/advices/GlobalExceptionHandler.java
@@ -0,0 +1,59 @@
+package com.code.prodapp.orderservice.advices;
+
+import com.code.prodapp.orderservice.exceptions.CustomerAddressNotFoundException;
+import com.code.prodapp.orderservice.exceptions.CustomerNotFoundException;
+import com.code.prodapp.orderservice.exceptions.GeocodingFailedException;
+import com.code.prodapp.orderservice.exceptions.OrderAlreadyCancelledException;
+import com.code.prodapp.orderservice.exceptions.OrderNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(OrderNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleOrderNotFound(OrderNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(Map.of("message", exception.getMessage()));
+    }
+
+    @ExceptionHandler(CustomerNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleCustomerNotFound(CustomerNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(Map.of("message", exception.getMessage()));
+    }
+
+    @ExceptionHandler(CustomerAddressNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleCustomerAddressNotFound(CustomerAddressNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(Map.of("message", exception.getMessage()));
+    }
+
+    @ExceptionHandler(OrderAlreadyCancelledException.class)
+    public ResponseEntity<Map<String, String>> handleOrderAlreadyCancelled(OrderAlreadyCancelledException exception) {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(Map.of("message", exception.getMessage()));
+    }
+
+    @ExceptionHandler(GeocodingFailedException.class)
+    public ResponseEntity<Map<String, String>> handleGeocodingFailed(GeocodingFailedException exception) {
+        return ResponseEntity.status(HttpStatus.BAD_GATEWAY)
+                .body(Map.of("message", exception.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidation(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+        for (FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return ResponseEntity.badRequest().body(errors);
+    }
+}
